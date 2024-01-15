9ec60021aad6a4283164f87b128dacd756848823
rilangki <rilangkishabong@gmail.com>
2024-01-16 01:32:39 +0530

    connected to database and implimented NextAuth authentication using google OAuth. Handled user signin and sessions accordingly. Every new email will be saved to the database.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { signIn,signOut,useSession,getProviders} from 'next-auth/react'
 
 const Nav = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
@@ -32,9 +32,10 @@ const Nav = () => {
         <p className='logo_text'>Promptopia</p>
       </Link>
 
+
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link
               href="/create-prompt"
@@ -50,7 +51,7 @@ const Nav = () => {
               Sign Out
             </button>
             <Link href='/profile'>
-              <Image src='/assets/images/logo.svg'
+              <Image src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -78,9 +79,9 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
-             <Image src='/assets/images/logo.svg'
+             <Image src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
